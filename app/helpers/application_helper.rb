e91ab4cb5374abf46893f020b0c8e64241de84c0
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-06-04 10:34:22 +0900

    ✨ Update default layout
    title用のヘルパーを追加、ヘッダーのパーシャルを追加、
    フラッシュメッセージのパーシャルを追加。
    なおデザインはあくまで暫定的なもの。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def build_title(page_title = "")
+    base_title = "タスクカズラ（仮）"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
 end
