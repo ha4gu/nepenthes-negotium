2dfc18e204d80be36ac0bd586e2ccf289206ebb5
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-06-04 14:34:52 +0900

    ✨ Implement tasks#show
    タスク詳細ページの実装。
    なお、指定されたIDのタスクが存在しない場合にはタスク一覧ページに
    リダイレクトするよう実装している。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,11 @@ class TasksController < ApplicationController
   end
 
   def show
+    @page_title = "タスク詳細"
+    @task = Task.find_by_id(params[:id])
+    if @task.nil?
+      redirect_to tasks_url, alert: "指定されたタスクは見つかりません。"
+    end
   end
 
   def new
