fc07fd9a6a38ed3d6f3f3520a817650cd44472a9
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-06-06 15:30:17 +0900

    ✨ Enable user to edit deadline of task
    タスクの終了期限を入力できるようにした。
    入力項目が不十分な場合には値は保存されない。
    また、日付の入力がなく時刻のみ入力されている場合には、
    日付は自動的にその日の日付となるようにした。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,7 +52,25 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:subject, :detail)
+    date_year   = params[:task]["deadline_date(1i)"]
+    date_month  = params[:task]["deadline_date(2i)"]
+    date_day    = params[:task]["deadline_date(3i)"]
+    time_hour   = params[:task]["deadline_time(4i)"]
+    time_minute = params[:task]["deadline_time(5i)"]
+
+    if date_year.present? && date_month.present? && date_day.present? && time_hour.present? && time_minute.present?
+      # すべての項目が入力済みの場合: _dateも_timeも有効
+      params.require(:task).permit(:subject, :detail, :deadline_date, :deadline_time)
+    elsif date_year.present? && date_month.present? && date_day.present?
+      # 日付部分は入力済みだが時刻部分は不十分: _dateのみ有効
+      params.require(:task).permit(:subject, :detail, :deadline_date)
+    elsif time_hour.present? && time_minute.present?
+      # 時刻部分は入力済みだが日付部分は不十分: _timeのみ有効
+      params.require(:task).permit(:subject, :detail, :deadline_time)
+    else
+      # 日付部分も時刻部分も不十分
+      params.require(:task).permit(:subject, :detail)
+    end
   end
 
   def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,19 @@
 class Task < ApplicationRecord
+  # Callback
+  before_validation :set_deadline_date
+
   # Validation
   validates :subject, presence: true
 
   # Order
   default_scope { order(created_at: :desc) }
+
+  private
+
+  # 終了期限日が空だが終了期限時刻は定まっている場合に、終了期限日を今日の日付にする処理
+  def set_deadline_date
+    if self.deadline_time.present? && !self.deadline_date.present?
+      self.deadline_date = Date.today
+    end
+  end
 end
