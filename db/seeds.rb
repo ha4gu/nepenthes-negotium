0c9f831006e03cf73bde25543c185cdaa2fc14eb
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-06-07 21:20:45 +0900

    ✅ Increase number of tasks on seeds
    seedsによるタスク数を50000件に増加させた。
    deadlineやstatusにも数値が入ったり入らなかったりしてある。

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-COUNT = 20 # should be less than or equal 100
-COUNT.times do |i|
-  current_subject = "Task#{sprintf "%03d", i+1}"
+COUNT = 50000 # should be less than or equal 99999
+1.step(COUNT) do |i|
+  current_subject = "Task#{sprintf "%05d", i}"
   Task.find_or_create_by!(subject: current_subject) do |t|
     t.subject = current_subject
-    t.detail  = "This is Task #{sprintf "%03d", i+1}"
-    t.created_at = t.updated_at =  Time.zone.now.ago((100-i).hours).ago((100-i).minutes)
+    t.detail  = "This is Task #{sprintf "%05d", i}"
+    t.deadline_date = Date.today.since(i.days).to_date if i % 3 == 0
+    t.deadline_time = Time.zone.now.since(i.minutes)   if i % 5 == 0
+    if i % 100 == 0
+      t.status = 2
+    elsif i % 10 == 0
+      t.status = 1
+    else
+      t.status = 0
+    end
+    t.created_at = t.updated_at =  Time.zone.now.ago((10000-i).minutes)
   end
 end
