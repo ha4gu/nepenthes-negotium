2f43fdfdfccdd914074e1fd2adc06d84136bcca5
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-06-14 13:41:15 +0900

    🐛 Change paths of user controller
    ユーザコントローラ配下の各URLのパスを変更した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "static_pages#top"
   resources :tasks
-  resources :users
+  resources :users, path: "admin/users"
   get "/login", to: "sessions#new"
   resources :sessions, only: [:create, :destroy]
 end
