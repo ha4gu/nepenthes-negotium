9ba299bb8d008c864f41a022462a2c9ab04eaa79
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-06-16 16:50:23 +0900

    ✨ Add function to prevent to lost all admins
    管理者が1人もいなくならないように更新や削除を制御するようにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,28 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX},
             uniqueness: { case_sensitive: false }
+
+  # Callback
+  before_update  :avoid_to_demote_last_admin
+  before_destroy :avoid_to_delete_last_admin
+
+  # Error
+  class LastAdminError < StandardError
+  end
+
+  private
+
+  # prevent to update the last admin to normal user
+  def avoid_to_demote_last_admin
+    if User.where(admin: true).size == 1 && !self.admin && self.admin_was
+      raise LastAdminError
+    end
+  end
+
+  # prevent to delete the last admin
+  def avoid_to_delete_last_admin
+    if User.where(admin: true).size == 1 && self.admin?
+      raise LastAdminError
+    end
+  end
 end
